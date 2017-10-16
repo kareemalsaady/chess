2be35454728d56789d2b11b55ab8f576b69887d6
Kareem Al Saady <kareem.alsaady@gmail.com>
2017-10-16 14:49:20 +0400

    complete pieces set up

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -144,11 +144,105 @@ class Bishop < Pieces
              moves << [x-5,y+5]
              moves << [x-6,y+6]
              moves << [x-7,y+7]
+
+             moves << [x,y+1]
+             moves << [x,y+2]
+             moves << [x,y+3]
+             moves << [x,y+4]
+             moves << [x,y+5]
+             moves << [x,y+6]
+             moves << [x,y+7]
+             moves << [x+1,y]
+             moves << [x+2,y]
+             moves << [x+3,y]
+             moves << [x+4,y]
+             moves << [x+5,y]
+             moves << [x+6,y]
+             moves << [x+7,y]
     @moves.reject! { |m| m[0]<0 || m[1]<0 || m[0]>7 || m[1]>7}
   end
 end
 
-t = Bishop.new(4,3)
+class Queen < Pieces
+  def initialize(x,y)
+    @x = x
+    @y = y
+    @current_position = ([x,y])
+    @color = "black"
+    @moves = []
+    @unicode = "\u265B"
+    @unicode_white = "\u2655"
+    @unicode_black = "\u265B"
+  end
+
+  def possible_moves
+    moves = []
+    @moves = moves << [x+1,y+1]
+             moves << [x+2,y+2]
+             moves << [x+3,y+3]
+             moves << [x+4,y+4]
+             moves << [x+5,y+5]
+             moves << [x+6,y+6]
+             moves << [x+7,y+7]
+
+             moves << [x-1,y-1]
+             moves << [x-2,y-2]
+             moves << [x-3,y-3]
+             moves << [x-4,y-4]
+             moves << [x-5,y-5]
+             moves << [x-6,y-6]
+             moves << [x-7,y-7]
+
+             moves << [x+1,y-1]
+             moves << [x+2,y-2]
+             moves << [x+3,y-3]
+             moves << [x+4,y-4]
+             moves << [x+5,y-5]
+             moves << [x+6,y-6]
+             moves << [x+7,y-7]
+
+             moves << [x-1,y+1]
+             moves << [x-2,y+2]
+             moves << [x-3,y+3]
+             moves << [x-4,y+4]
+             moves << [x-5,y+5]
+             moves << [x-6,y+6]
+             moves << [x-7,y+7]
+
+
+    @moves.reject! { |m| m[0]<0 || m[1]<0 || m[0]>7 || m[1]>7}
+  end
+end
+
+class King < Pieces
+  def initialize(x,y)
+    @x = x
+    @y = y
+    @current_position = ([x,y])
+    @color = "black"
+    @moves = []
+    @unicode = "\u265A"
+    @unicode_white = "\u2654"
+    @unicode_black = "\u265A"
+  end
+
+  def possible_moves
+    moves = []
+    @moves = moves << [x,y+1]
+             moves << [x+1,y+1]
+             moves << [x+1,y]
+             moves << [x+1,y-1]
+             moves << [x,y-1]
+             moves << [x-1,y-1]
+             moves << [x-1,y]
+             moves << [x-1,y+1]
+
+    @moves.reject! { |m| m[0]<0 || m[1]<0 || m[0]>7 || m[1]>7}
+  end
+end
+
+
+t = King.new(4,3)
 t.possible_moves
 p t
 puts t.unicode
